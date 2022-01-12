ba6a96be16054a44a860dd12e0ceb912217d7a20
ukamal <ukamal@isabelgroup.eu>
2022-01-12 23:37:08 +0100

    feat(Restfull Endpoint): Basic authentication with Keycloak test/test user with KC_Role.

--- src/main/java/be/techfix/learning/rest/HelloWorld.java
+++ src/main/java/be/techfix/learning/rest/HelloWorld.java
@@ -1,9 +1,11 @@
 package be.techfix.learning.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloWorld {
     @GetMapping("/hello")
     public String hello() {
